70628895dbb0dcdfd6675b9cfbc94d37c158c4b4
Aravind_PK <aravind.pk@iqvia.com>
2021-08-04 16:07:10 +0530

    first commit

---
+++ DAY3_JAVASCRIPT/myjavascript.js
@@ -0,0 +1,11 @@
+
+
+
+console.log("inside the my javascript file")
+
+document.getElementById("externalid").style.color = 'blue';
+document.getElementById("externalid").style.background.color = 'orange';
+
+function clickMe() {
+    document.getElementById("externalid").style.backgroundColor = 'tomato';
+}
